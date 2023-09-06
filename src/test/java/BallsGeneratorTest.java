8602926d7960f88dd33e99e5dd97a5f4656e6679
Kyuyoung11 <kyuyoung.lee@bankwareglobal.com>
2023-09-06 12:58:17 +0900

    Balls - balls 변환 기능 추가

--- src/test/java/BallsGeneratorTest.java
+++ src/test/java/BallsGeneratorTest.java
@@ -17,4 +17,12 @@ public class BallsGeneratorTest {
         Balls balls = ballsGenerator.makeRandomBalls(3);
         assertEquals(balls.getBalls().size(),3);
     }
+
+    @Test
+    void convertFromStringToBallsTest() {
+        Balls balls = ballsGenerator.convertFromStringToBalls("123");
+        assertEquals(balls.getBalls().get(0).getPosition(),1);
+        assertEquals(balls.getBalls().get(0).getNumber(),1);
+
+    }
 }
