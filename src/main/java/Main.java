92f9ef65999dd2e7ebadb6ae8c07782619225068
Kyuyoung11 <kyuyoung.lee@bankwareglobal.com>
2023-09-06 12:58:32 +0900

    main 기능 합치기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,23 @@
 import java.util.LinkedHashSet;
 
 public class Main {
-    private static BallsGenerator ballsGenerator = new BallsGenerator();
+    private static InputView inputView;
+    private static BallsGenerator ballsGenerator;
+    private static final int SIZE = 3;
     public static void main(String[] args) {
-        //1. 난수 생성
-        Balls balls = ballsGenerator.makeRandomBalls(3);
+        gameStart();
+    }
 
-        //2. 사용자 숫자 입력 받기
+    public static void gameStart() {
+        //1.난수생성
+        Balls randomBalls = ballsGenerator.makeRandomBalls(SIZE);
+
+        //2. 사용자 숫자 입력
+        String numString = inputView.getNumberInput();
+        Balls userBalls = ballsGenerator.convertFromStringToBalls(numString);
+
+        //3. 정답과 비교
+
 
     }
 }
