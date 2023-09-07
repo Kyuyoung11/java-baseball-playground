ee2a83d1d862f2823cbb5459e44c963f75f6ae7f
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-09-08 00:41:44 +0900

    볼 분류 기능 추가

--- src/main/java/Balls.java
+++ src/main/java/Balls.java
@@ -17,14 +17,29 @@ public class Balls {
     }
 
     public void addBall(Integer ballNumber) {
-        if(!isDuplicateBall(ballNumber)) {
+        if(!isBall(ballNumber)) {
             Ball newBall = new Ball(this.balls.size()+1, ballNumber);
             this.balls.add(newBall);
         }
     }
 
-    public boolean isDuplicateBall(Integer ballNumber) {
+    public boolean isBall(Integer ballNumber) {
         return this.balls.stream()
                 .anyMatch(ball -> ball.getNumber() == ballNumber);
+    }
+
+    public boolean isStrike(Ball ball) {
+        return this.balls.get(ball.getPosition()-1).getNumber() == ball.getNumber();
+    }
+
+    public BallStatus getBallStatus(Ball ball) {
+        if (isStrike(ball)) {
+            return BallStatus.STRIKE;
+        }
+        if (isBall(ball.getNumber())) {
+            return BallStatus.BALL;
+        }
+        return BallStatus.NOTHING;
+
     }
 }
