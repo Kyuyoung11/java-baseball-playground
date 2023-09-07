f5ed0f32895d6713c611aec98135ddfd16da9205
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-09-08 00:42:43 +0900

    결과 비교 기능 추가

--- src/main/java/ResultChecker.java
+++ src/main/java/ResultChecker.java
@@ -1,20 +1,39 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ResultChecker {
 
     public boolean compareAnswer(Balls answerBalls, Balls tryBalls) {
-        Map<BallStatus, Integer> counter = new HashMap<>();
+        List<BallStatus> ballStatusList = countBalls(answerBalls, tryBalls);
 
-        BallClassifier ballClassifier = new BallClassifier();
-        for (Ball ball : tryBalls.getBalls()) {
-            BallStatus ballStatus = ballClassifier.classifyBallStatus(answerBalls, ball);
-            counter.put(ballStatus, counter.get(ballStatus)+1);
+        printResult(ballStatusList);
+
+        return isAnswer(ballStatusList);
+
+    }
+
+    private void printResult(List<BallStatus> ballStatusList) {
+        if (Collections.frequency(ballStatusList, BallStatus.NOTHING) == 3) {
+            System.out.println("낫싱");
+            return;
         }
 
+        if (Collections.frequency(ballStatusList, BallStatus.BALL) > 0) {
+            System.out.print(Collections.frequency(ballStatusList, BallStatus.BALL) + "볼 ");
+        }
+        if (Collections.frequency(ballStatusList, BallStatus.STRIKE) > 0) {
+            System.out.print(Collections.frequency(ballStatusList, BallStatus.STRIKE) + "스트라이크");
+        }
+        System.out.println();
+    }
 
+    private boolean isAnswer(List<BallStatus> ballStatusList) {
+        return Collections.frequency(ballStatusList, BallStatus.STRIKE) == 3;
+    }
 
-        return counter.get(BallStatus.STRIKE) == 3;
-
+    public List<BallStatus> countBalls(Balls answerBalls, Balls tryBalls) {
+        return tryBalls.getBalls().stream()
+                .map(answerBalls::getBallStatus)
+                .collect(Collectors.toList());
     }
 }
